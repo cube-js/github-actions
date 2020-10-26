388c162e3cfa8bfa64697771a2a87585aef7a9c1
Dmitry Patsura <talk@dmtry.me>
2020-10-26 20:44:25 +0300

    feat(author-detector): Initial version

---
+++ author-detector/index.ts
@@ -0,0 +1,66 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github';
+
+const getOrganizationMembers = async () => {
+    return [
+        'igorlukanin',
+        'jkotova',
+        'keydunov',
+        'levhav',
+        'ovr',
+        'paveltiunov',
+        'rpaik',
+        'RusovDmitriy',
+        'tenphi',
+        'vasilev-alex',
+        'YakovlevCoded'
+    ].map(
+        (v) => v.toLowerCase()
+    );
+};
+
+const getRequiredInput = (name: string) => {
+    const value = core.getInput('token');
+    if (value) {
+        return value;
+    }
+
+    throw new Error(
+        `Unable to get require input parameter: ${name}`,
+    );
+}
+
+async function run(): Promise<void> {
+  try {
+    const api = github.getOctokit(
+        getRequiredInput('token')
+    );
+
+    const { data: issue } = await api.issues.get({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: github.context.issue.number,
+    });
+
+    if (issue.user.login) {
+        const team = await getOrganizationMembers();
+        const label = getRequiredInput(
+            team.includes(issue.user.login.toLowerCase()) ? 'coreLabel' : 'communityLabel'
+        );
+
+        api.issues.addLabels({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: github.context.issue.number,
+            labels: [
+                label,
+            ],
+        });
+    }
+
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run();
